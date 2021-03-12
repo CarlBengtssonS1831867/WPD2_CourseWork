ee900340b1ede4a20a1440e0cc82f23478c7c2b9
CarlBengtssonS1831867 <cbengt200@caledonian.ac.uk>
2021-03-12 22:13:33 +0000

    Added visual update of database. Now shows activities on schedule.

--- controllers/dashboardControllers.js
+++ controllers/dashboardControllers.js
@@ -1,3 +1,6 @@
+const databaseDAO = require('../models/dashboardModel')
+const db = new databaseDAO('database.db')
+
 exports.login = function(req, res) {
     res.render('login', {
         'title': 'Login'
@@ -13,11 +16,79 @@ exports.editWeek = function(req, res) {
 }
 
 exports.postEditWeek = function(req, res) {
+    db.insertNewActivity(req.body.Date, req.body.Activity, req.body.Number, req.body.Unit)
     res.redirect('/Plan')
 }
 
 exports.plan = function(req, res) {
-    res.sendFile(public + '/plan.html')
+
+    db.selectAllEntries().then((list) => {
+        
+        var monday = [], tuesday = [], wednesday = [], thursday = [], friday = [], saturday = [], sunday = []
+        
+        list.forEach(element => {
+            var date = element.date.toISOString()
+            console.log(date)
+            switch(date) {
+                case '2021-03-08T00:00:00.000Z':
+                    monday.push(element)
+                    break
+                case '2021-03-09T00:00:00.000Z':
+                    tuesday.push(element)
+                    break
+                case '2021-03-10T00:00:00.000Z':
+                    wednesday.push(element)
+                    break
+                case '2021-03-11T00:00:00.000Z':
+                    thursday.push(element)
+                    break
+                case '2021-03-12T00:00:00.000Z':
+                    friday.push(element)
+                    break
+                case '2021-03-13T00:00:00.000Z':
+                    saturday.push(element)
+                    break
+                case '2021-03-14T00:00:00.000Z':
+                    sunday.push(element)
+                    break
+            }
+        })
+
+        console.log('monday', monday)
+        console.log('friday', friday)
+
+        res.render('plan', {
+            'title': 'Plan',
+            'Monday': monday,
+            'Tuesday': tuesday,
+            'Wednesday': wednesday,
+            'Thursday': thursday,
+            'Friday': friday,
+            'Saturday': saturday,
+            'Sunday': sunday
+        })
+    })
+    
+    /* wednesday = db.selectEntriesOnDate(new Date('10 Mar 2021'))
+    thursday = db.selectEntriesOnDate(new Date('11 Mar 2021'))
+    friday = db.selectEntriesOnDate(new Date('12 Mar 2021'))
+    saturday = db.selectEntriesOnDate(new Date('13 Mar 2021'))
+    sunday = db.selectEntriesOnDate(new Date('14 Mar 2021')) */
+
+    // console.log(Monday)
+
+    /* res.render('plan', {
+        'title': 'Plan',
+        'Monday': monday,
+        'Tuseday': tuesday,
+        'Wednesday': wednesday,
+        'Thursday': thursday,
+        'Friday': friday,
+        'Saturday': saturday,
+        'Sunday': sunday
+    }) */
+
+    //res.sendFile(public + '/plan.html')
 }
 
 exports.notImplemented = function(req, res) {
---
+++ models/dashboardModel.js
@@ -0,0 +1,59 @@
+const nedb = require('nedb')
+
+class ExcersisePlan {
+    constructor(dbFilePath) {
+        if (dbFilePath) {
+            this.db = new nedb({filename: dbFilePath, autoload: true})
+            console.log('Database loaded from:', dbFilePath)
+        } else {
+            this.db = new nedb()
+            console.log('Database loaded in memory')
+        }
+    }
+
+    insertNewActivity(date, activity, reps, unit) {
+        var entry = {
+            date: new Date(date),
+            activity: activity,
+            reps, reps,
+            unit: unit
+        }
+
+        this.db.insert(entry, function(err, doc) {
+            if (err) {
+                console.log('Error: ', err)
+            } else {
+                console.log('Document Inserted: ', doc)
+            }
+        })
+    }
+
+    selectAllEntries() {
+        return new Promise((resolve, reject) => {
+            this.db.find({}, function(err, entries) {
+                if (err) {
+                    reject(err)
+                } else {
+                    resolve(entries)
+                    console.log('entries: ', entries)
+                }
+            })
+        })
+    }
+
+    selectEntriesOnDate(date) {
+        return new Promise((resolve, reject) => {
+            this.db.find({date: date}, function(err, entries) {
+                if (err) {
+                    reject(err)
+                } else {
+                    resolve(entries)
+                    console.log('entries: ', entries)
+                }
+            })
+        })
+    }
+
+}
+
+module.exports = ExcersisePlan
